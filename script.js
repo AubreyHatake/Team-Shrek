99c861c0edb835a2313102515c3e12040b1d9381
Conneroc03 <122755260+Conneroc03@users.noreply.github.com>
2023-03-30 20:27:49 -0600

    button

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById('button');
+button.addEventListener('click', function() {
+    console.log('button clicked');
+    });
+// Path: index.html
+<button id="button">Click me</button>
