00fd2329335357c52a07ee15fb68e0a8e21fcb3f
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-04-03 19:12:54 -0600

    input box added

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-var button = document.getElementById('button');
-button.addEventListener('click', function() {
-    console.log('button clicked');
-    });
+// var button = document.getElementById('button');
+// button.addEventListener('click', function() {
+//     console.log('button clicked');
+//     });
 // Path: index.html
-<button id="button">Click me</button>
+/* <button id="button">Click me</button> */
+
+var btn = document.querySelector('#search-artist');
+
+
+function x () {
+    console.log("you clicked a button!")
+};
+
+btn.addEventListener("click", x);
 
 $( function() {
     $( "#datepicker" ).datepicker();
