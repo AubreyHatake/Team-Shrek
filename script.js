65a7e1c0ad872997189fff2dbbceb20b1e3819f7
shru <shruthishekar@gmail.com>
2023-04-03 21:26:36 -0600

    changed concert to hike

--- script.js
+++ script.js
@@ -5,15 +5,47 @@
 // Path: index.html
 /* <button id="button">Click me</button> */
 
-var btn = document.querySelector('#search-artist');
+// var btn = document.querySelector('#search-artist');
 
 
-function x () {
-    console.log("you clicked a button!")
-};
+// function x () {
+    // console.log("you clicked a button!")
+// };
 
-btn.addEventListener("click", x);
+// btn.addEventListener("click", x);
 
-$( function() {
+
+
+/*var searchArtistBtnEl = $("#search-artist");
+
+
+$("#search-artist").on("click",function (event) 
+{
+    event.preventDefault();
+    var artistNameInputEl = $("#artistInput").val();
+    console.log(artistNameInputEl);
+    //alert("artist name")
+    if(artistNameInputEl === "")
+    {
+        alert("Please Enter valid Artist name ");
+    }
+    else
+    {
+        
+        getArtistName(artistNameInputEl);
+        $("#artistInput").val("");    
+    }
+});*/
+
+
+ 
+
+
+
+
+
+
+
+/*$( function() {
     $( "#datepicker" ).datepicker();
-  } );
+  } );*/
